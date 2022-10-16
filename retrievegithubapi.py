caa372d0ad49867b01922f8aa23c594e3dc8559f
Ameya Sathe <amej@users.noreply.github.com>
2022-10-16 08:02:35 +0530

    -Adds and refactor functions to   - retrieve github pull requests matching specific state   - filter specific fields   - generate csv report
    Signed-off-by: Ameya Sathe <amej@users.noreply.github.com>

--- retrievegithubapi.py
+++ retrievegithubapi.py
@@ -1,4 +1,5 @@
-"""Module providing Python library to use the Github API v3."""
+"""Module  providing Python library to use the Github API v3."""
+import csv
 import os
 from datetime import datetime, timedelta
 from github import Github
@@ -8,31 +9,58 @@ def search_for_pr(gitcreds, gitrepo, prstatus, startdate):
     """Function to retrieve pr"""
 
     repository = gitcreds.get_repo(gitrepo)
-    print("Name of repository is ")
-    print(repository)
-    print(startdate)
-    startrange = startdate.strftime("%Y-%m-%d")
-    query = "type:pr"+" state:"+prstatus+" repo:" + \
-        gitrepo+" created:"+startrange+"..*"
-    print(query)
-    pulls = gitcreds.search_issues(query, sort='created', order='asc')
-    for pull_request in pulls:
-        print(pull_request.number, pull_request.title)
+    print("Name of repository is "+str(repository))
+    print("Searching from"+str(startdate))
+    stringified_date = startdate.strftime("%Y-%m-%d")
+    if prstatus is not "merged":
+        query = "type:pr"+" state:"+prstatus+" repo:" + \
+                gitrepo+" created:>"+stringified_date
+    else:
+        query = "type:pr"+" is:"+prstatus+" repo:" + \
+                gitrepo+" created:>"+stringified_date
+    print("Executing search as"+query)
+    pulls = gitcreds.search_issues(query, sort='updated', order='asc')
+    return pulls
 
 
-# def retrieve_pr_title(gitcreds,gitrepo,prstatus):
-#    """Function to retrieve pr title"""
+def append_report(report_name, collection_of_pr_fields):
+    """Function accepts collection of PR  and prints out its report"""
+    report = open(report_name, 'a+', newline='',encoding='utf-8')
+    csvwriter = csv.writer(report)
+    if os.path.getsize(report_name) == 0:
+        csvwriter.writerow(['ID of the Pull Request', 'Title of the Pull Request',
+                            'Opened on', 'Last Updated', 'Closed at'])
+    for row in collection_of_pr_fields:
+        csvwriter.writerow([row['state'], row['id'], row['title'],
+                            row['opened_on'], row['last_updated_date'], row['closed_on']])
+    report.close()
+    print('Full Report complete')
 
- #   repo = gitcreds.get_repo(gitrepo)
-  #  print("Name of repository is ")
-   # print(repo)
-    #pulls = repo.get_pulls(state=prstatus, sort='created', base='master')
-    # for each_pr in pulls:
-     #   print (each_pr)
+
+def get_specific_fields_of_pr(prdatadump):
+    """Function accepts all fields of  github PR and returns the number,title etc"""
+    cleaned_list = []
+    for pull_request in prdatadump:
+        if pull_request is not None:
+            cleaned_item = {}
+            cleaned_item['state'] = pull_request.state
+            cleaned_item['id'] = pull_request.number
+            cleaned_item['title'] = pull_request.title
+            cleaned_item['opened_on'] = pull_request.created_at
+            cleaned_item['last_updated_date'] = pull_request.updated_at
+            cleaned_item['closed_on'] = pull_request.closed_at
+            cleaned_list.append(cleaned_item)
+    return cleaned_list
 
 
 def main():
     """Launcher function"""
+
+    report_time = datetime.now
+    report_name = 'GitHub_PullRequest'+f'{report_time}:%Y-%m-%d_%H%M'+'.csv'
+    report_dir = '/var/tmp/'
+    report_name = report_dir + '/' + report_name
+
     # using an access token
     access_token = os.environ.get("GITHUB_TOKEN")
     # Pass the base_url and login
@@ -45,12 +73,13 @@ def main():
 
     # Sample repo
     pub_repo = os.environ.get("PUBLIC_REPO")
-    state = ["open", "closed"]
+    state = ["merged", "closed", "open"]
 
     for prstate in state:
-        search_for_pr(github_creds, pub_repo, prstate, startdate)
-    # retrieve_pr_title(github_creds,SAMPLE,"open")
-
+        resulting_prs = search_for_pr(
+            github_creds, pub_repo, prstate, startdate)
+        prstate_fields = get_specific_fields_of_pr(resulting_prs)
+        append_report(report_name, prstate_fields)
 
 # write code that will use the GitHub API to
 # retrieve a summary of all opened, closed, and in progress pull requests
